fc3b9a5a4de8b1e530dbb671adc31c532dd68fd8
Chinthala Rajasekhara Reddy <68636215+rajareddy4556@users.noreply.github.com>
2023-06-02 01:05:46 +0530

    yes commit

---
+++ script.js
@@ -0,0 +1,64 @@
+window.onload = function() {
+    document.getElementById("home").style.marginTop="10px";
+    document.getElementById("aboutme").style.display = "none";
+    document.getElementById("b1").style.borderRadius="20%"
+    document.getElementById("b1").style.borderBottom="2px solid red";
+    document.getElementById("contactme").style.display = "none";
+    document.getElementById("projects").style.display = "none";
+}
+home =() =>{
+    document.getElementById("home").style.marginTop="10px";
+    document.getElementById("home").style.display="flex";
+    document.getElementById("home").style.justifyContent = "center";
+    document.getElementById("aboutme").style.display="none";
+    document.getElementById("b2").style.borderBottom="none";
+    document.getElementById("b1").style.borderBottom="2px solid red";
+    document.getElementById("b1").style.borderRadius="20%";
+    document.getElementById("b4").style.borderBottom="none";
+    document.getElementById("contactme").style.display = "none";
+    document.getElementById("b3").style.borderBottom="none";
+    document.getElementById("projects").style.display="none";
+}
+aboutme =() =>{
+    document.getElementById("aboutme").style.marginTop="10px";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("b1").style.borderBottom="none";
+    document.getElementById("b2").style.borderBottom="2px solid red";
+    document.getElementById("b2").style.borderRadius="20%"
+    document.getElementById("aboutme").style.display = "flex";
+    document.getElementById("aboutme").style.justifyContent = "center";
+    document.getElementById("b4").style.borderBottom="none";
+    document.getElementById("contactme").style.display = "none";
+    document.getElementById("b3").style.borderBottom="none";
+    document.getElementById("projects").style.display="none";
+}
+
+projects =() => {
+    document.getElementById("b3").style.borderBottom="2px solid red";
+    document.getElementById("b3").style.borderRadius="20%";
+    document.getElementById("b1").style.borderBottom="none";
+    document.getElementById("b2").style.borderBottom="none";
+    document.getElementById("b4").style.borderBottom="none";
+    document.getElementById("projects").style.display="flex";
+    document.getElementById("projects").style.justifyContent="center";
+    document.getElementById("home").style.display="none";
+    document.getElementById("contactme").style.display="none";
+    document.getElementById("aboutme").style.display="none";
+
+}
+
+
+
+contactme =() => {
+    document.getElementById("b1").style.borderBottom="none";
+    document.getElementById("b2").style.borderBottom="none";
+    document.getElementById("b4").style.borderBottom="2px solid red";
+    document.getElementById("b4").style.borderRadius="20%";
+    document.getElementById("home").style.display = "none";
+    document.getElementById("aboutme").style.display="none";
+    document.getElementById("contactme").style.display="flex";
+    document.getElementById("contactme").style.justifyContent = "center";
+    document.getElementById("b3").style.borderBottom="none";
+    document.getElementById("projects").style.display="none";
+
+}
